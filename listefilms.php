c14193ae1d756d75940f5469f287367c870d2114
Valentin739 <117243916+Valentin739@users.noreply.github.com>
2023-03-08 15:37:41 +0100

    Update listefilms.php

--- listefilms.php
+++ listefilms.php
@@ -7,7 +7,7 @@
 //Lister le contenu de la table FILMS
 
 //1° - Connexion à la BDD
-$base = new PDO('mysql:host=localhost; dbname=id20215400_cnamgreta', 'id20215400_boris', '=nq^^=vKz?FRiz7-');
+$base = new PDO('mysql:host=localhost; dbname=id20406509_valoubdz', 'id20406509_valoubd', 'VKag13p'');
 $base->exec("SET CHARACTER SET utf8");
 
 //2° - Préparation de requette et execution
